24336c4511c0ddb2322eabbd449bc01173223d83
Heulem Barros <heulembarros@gmail.com>
2022-06-27 22:59:24 -0300

    03.Benefícios da tipagem estática

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,7 @@
-import { Negociacao } from "./models/negociacao.js";
-const negociacao = new Negociacao(new Date(), 1000, 100);
-console.log(negociacao.valor);
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
